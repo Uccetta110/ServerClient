81bc853fe6924c0674386af7ccc34cd07179cd5a
AGLIATI FEDERICO <AGLIATI.21076>
2025-11-11 11:53:13 +0100

    client test

---
+++ ClientSocket/ClientSocket/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+public class SynchronousSocketClient
+{
+    static string localIpAddressInput;
+
+    static string GetLocalIPv4()
+    {
+        foreach (var ip in Dns.GetHostAddresses(Dns.GetHostName()))
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork) // Filtra solo IPv4
+            {
+                return ip.ToString();
+            }
+        }
+        return null; // Nessun IPv4 trovato
+    }
+
+    static public async void clientTest(Socket handler)
+    {
+        await Task.Delay(5000); // Aspetta per 5 secondi
+
+        // Invia un messaggio di test al client
+        string testMessage = "Messaggio di test dal client " + localIpAddressInput + "<EOF>";
+        byte[] msg = Encoding.ASCII.GetBytes(testMessage);
+
+        // Assicurati di avere accesso al socket handler
+        // Potresti dover passare il socket handler come parametro a questa funzione
+        handler.Send(msg);
+    }
+
+    public static void StartClient(string ipAddressInput, string stringSend)
+    {
+        // Data buffer for incoming data.  
+        byte[] bytes = new byte[1024];
+
+        // Connect to a remote device.  
+        try
+        {
+            // Establish the remote endpoint for the socket.  
+            // This example uses port 11000 on the local computer.  
+            IPAddress ipAddress = System.Net.IPAddress.Parse(ipAddressInput);
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, 5000);
+
+            // Create a TCP/IP  socket.  
+            Socket sender = new Socket(ipAddress.AddressFamily,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            // Connect the socket to the remote endpoint. Catch any errors.  
+            try
+            {
+                sender.Connect(remoteEP);
+
+                Console.WriteLine("Socket connected to {0}",
+                    sender.RemoteEndPoint.ToString());
+
+                // Encode the data string into a byte array.  
+                byte[] msg = Encoding.ASCII.GetBytes(stringSend + "<EOF>");
+
+                // Send the data through the socket.  
+                int bytesSent = sender.Send(msg);
+
+                // Receive the response from the remote device.  
+                int bytesRec = sender.Receive(bytes);
+                Console.WriteLine("Echoed test = {0}",
+                    Encoding.ASCII.GetString(bytes, 0, bytesRec));
+
+                // Release the socket.  
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+
+            }
+            catch (ArgumentNullException ane)
+            {
+                Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine("SocketException : {0}", se.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected exception : {0}", e.ToString());
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+
+    public static int Main(String[] args)
+    {
+        AppDomain.CurrentDomain.ProcessExit += OnProcessExit;
+        Console.WriteLine("Vuoi usare l'indirizzo IP locale? (s/n)");
+        string confirmation = Console.ReadLine();
+        string ipAddressInput;
+        if (confirmation.ToLower() == "s")
+        {
+            ipAddressInput = GetLocalIPv4();
+        }
+        else
+        {
+            Console.WriteLine("Inserisci l'indirizzo IP: ");
+            ipAddressInput = Console.ReadLine();
+            while (true)
+            {
+                Console.WriteLine("L'indirizzo IP è giusto: ");
+                Console.WriteLine(ipAddressInput + " (s/n)");
+                string confirmation1 = Console.ReadLine();
+                if (confirmation1.ToLower() == "s")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Inserisci l'indirizzo IP corretto: ");
+                    ipAddressInput = Console.ReadLine();
+                }
+            }
+        }
+        localIpAddressInput = ipAddressInput;
+
+
+        while (true)
+        {
+            Console.WriteLine("Inserisci la stringa da mandare al server: ");
+            string stringSend = Console.ReadLine();
+            StartClient(localIpAddressInput, stringSend);
+            Console.WriteLine("Premere un tasto per continuare ");
+            Console.ReadLine();
+        }
+
+        return 0;
+    }
+
+    static void OnProcessExit(object sender, EventArgs e)
+    {
+        // Invia il messaggio finale
+        StartClient(localIpAddressInput, "TERMINA CONNESSIONE");
+    }
+}
---
+++ ServerSocket/ServerSocket/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+public class SynchronousSocketListener
+{
+
+    // Incoming data from the client.  
+    public static string data = null;
+    public static bool connection = false;
+    public static bool connectionTest = false;
+    public static string localIPv4 = "";
+
+    static string GetLocalIPv4()
+    {
+        foreach (var ip in Dns.GetHostAddresses(Dns.GetHostName()))
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork) // Filtra solo IPv4
+            {
+                return ip.ToString();
+            }
+        }
+        return null; // Nessun IPv4 trovato
+    }
+
+    static async void clientTest(Socket listener)
+    {
+        if (connection == false)
+            return;
+        await Task.Delay(5000); // Aspetta per 5 secondi
+        if (connectionTest == false)
+        {
+            Console.WriteLine("Tempo di attesa dal client terminato. Nessuna connessione ricevuta.");
+            connection = false;
+        }
+        else
+        {
+            connectionTest = false; 
+        }
+    }
+
+    public static void StartListening()
+    {
+        
+        // Data buffer for incoming data.  
+        byte[] bytes = new Byte[1024];
+
+        // Establish the local endpoint for the socket.  
+        // Dns.GetHostName returns the name of the   
+        // host running the application.  
+
+        IPAddress ipAddress = System.Net.IPAddress.Parse(localIPv4);
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 5000);
+
+        // Create a TCP/IP socket.  
+        Socket listener = new Socket(ipAddress.AddressFamily,
+            SocketType.Stream, ProtocolType.Tcp);
+
+        // Bind the socket to the local endpoint and   
+        // listen for incoming connections.  
+        try
+        {
+            listener.Bind(localEndPoint);
+            listener.Listen(10);
+
+            // Start listening for connections.  
+            while (true)
+            {
+                if (connection == false)
+                Console.WriteLine("Waiting for a connection...");
+                // Program is suspended while waiting for an incoming connection.  
+                Socket handler = listener.Accept();
+                data = null;
+                connection = true;
+                connectionTest = true;
+
+                // An incoming connection needs to be processed.  
+                while (true)
+                {
+                    int bytesRec = handler.Receive(bytes);
+                    data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                    if (data.IndexOf("<EOF>") > -1)
+                    {
+                        break;
+                    }
+                }
+
+                if (data == "Messaggio di test dal client " + localIPv4 + "<EOF>")
+                    connectionTest = true;
+                // Show the data on the console.  
+                Console.WriteLine("Text received : {0}", data);
+
+                // Echo the data back to the client.  
+                byte[] msg = Encoding.ASCII.GetBytes(data);
+
+                handler.Send(msg);
+                if (data.ToUpper()=="TERMINA CONNESSIONE<EOF>")
+                {
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                    connection = false;
+                    connectionTest = false;
+                }
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+        Console.WriteLine("\nPress ENTER to continue...");
+        Console.Read();
+
+    }
+
+    public static int Main(String[] args)
+    {
+        localIPv4 = GetLocalIPv4();
+        StartListening();
+        return 0;
+    }
+}
